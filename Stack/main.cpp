d23aa3696401227a75df3321925548a871475373
Benjamin Harrison <ben@fuddlebunker.com>
2014-10-10 00:05:16 -0700

    Initial Commit

---
+++ Stack/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stack
+//
+//  Created by Fuddlebunker on 10/10/14.
+//  Copyright (c) 2014 Benjamin Harrison. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
